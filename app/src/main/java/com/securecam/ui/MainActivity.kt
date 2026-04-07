7a0dde45d44ed7d3466d51fbef3193da73436aa2
hemanthdvng <hemanthdvng1@gmail.com>
2026-04-07 23:47:13 +0530

    feat(ui): Setup MainActivity with Compose Navigation & Hilt

---
+++ app/src/main/java/com/securecam/ui/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.securecam.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.securecam.ui.screens.MainScreen
+import com.securecam.ui.screens.CameraScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val navController = rememberNavController()
+                    
+                    NavHost(navController = navController, startDestination = "main") {
+                        composable("main") { MainScreen(navController) }
+                        composable("camera") { CameraScreen(navController) }
+                    }
+                }
+            }
+        }
+    }
+}
